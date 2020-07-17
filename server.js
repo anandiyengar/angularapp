9d0ffa0e0e8bc5ae781cb2c1954760bcd5b93667
anand iyengar <anandiyengar0@gmail.com>
2020-07-17 14:27:54 +0530

    FIRST COMMIT 1.12

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/frontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
